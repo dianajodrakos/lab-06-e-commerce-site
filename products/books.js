efa663e6101a28bf7553b0fc25a269e6fbc46fe6
DJ Drakos <dianajodrakos@gmail.com>
2021-05-10 15:13:27 -0700

    added product arrays

---
+++ products/books.js
@@ -0,0 +1,98 @@
+const book1 = {
+    id: goodWork,
+    title: 'Good Work',
+    author: 'E. F. Schumacher',
+    image: 'good-work.jpeg',
+    description: '',
+    pages: 223,
+    year: 1979,
+    language: 'English',
+    publisher: 'HarperCollins Publishers',
+    ISBN: '0060905611',
+    category: 'theory',
+    price: 2.06,
+    };
+
+const book2 = {
+    id: labyrinths,
+    title: 'Labyrinths',
+    author: 'Jorge Luis Borges',
+    image: 'labyrinths.jpg',
+    description: '',
+    pages: 240,
+    year: 1962,
+    language: 'English',
+    publisher: 'New Directions',
+    ISBN: '0811216993',
+    category: 'fiction',
+    price: 2.26,
+    };
+
+const book3 = {
+    id: operating-spaceship-earth,
+    title: 'Operating Manual for Spaceship Earth',
+    author: 'R. Buckminster Fuller',
+    image: 'operating-spaceship-earth.jpg',
+    description: '',
+    pages: 152,
+    year: 1969,
+    language: 'English',
+    publisher: 'Lars Muller Publishers',
+    ISBN: '0935754016',
+    category: 'theory',
+    price: 6.00,
+    };
+
+const book4 = {
+    id: the-disposessed,
+    title: 'The Disposessed: An Ambiguous Utopia',
+    author: 'Ursula K. LeGuin',
+    image: 'the-disposessed.jpg',
+    description: '',
+    pages: 387,
+    year: 1974,
+    language: 'English',
+    publisher: 'HarperCollins Publishers',
+    ISBN: '006051275X',
+    category: 'fiction',
+    price: '$5.49',
+    };
+
+const book5 = {
+    id: simians-cyborgs-women,
+    title: 'Simians, Cyborgs, and Women: The Reinvention of Nature',
+    author: 'Donna Haraway',
+    image: 'simians-cyborgs-women.jpg',
+    description: '',
+    pages: 312,
+    year: 1991,
+    language: 'English',
+    publisher: 'Routledge',
+    ISBN: '0415903874',
+    category: 'theory',
+    price: 32.75,
+    };
+
+const book6 = {
+    id: fermentation-as-metaphor,
+    title: 'Fermentation As Metaphor',
+    author: 'Sandor Ellix Katz',
+    image: 'fermentation-as-metaphor.jpg',
+    description: '',
+    pages: 128,
+    year: 2020,
+    language: 'English',
+    publisher: 'Chelsea Green Publishing',
+    ISBN: '1645020215',
+    category: 'foodways',
+    price: 25.00,
+    };
+
+export const books = [
+    book1,
+    book2,
+    book3,
+    book4,
+    book5,
+    book6
+];
